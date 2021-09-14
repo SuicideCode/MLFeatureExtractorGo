d29c287614282b2097aaeb57b4fab7dec05322f8
SuicideCode <113363871+SuicideCode@users.noreply.github.com>
2021-09-14 09:12:50 +0000

    Upgrade npm packages

--- boolean.go
+++ boolean.go
@@ -6,3 +6,7 @@ import (
 	"fmt"
 
 	"github.com/spaolacci/murmur3"
+)
+
+// ExtractBoolean returns a 0/1 vector for the deterministic feature ID
+func ExtractBoolean(field, boolean string) (string, error) {
