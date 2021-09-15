9ced3817b48bf0ec83a104e3fd43ee61fc151c9b
SuicideCode <113363871+SuicideCode@users.noreply.github.com>
2021-09-15 06:45:16 +0000

    Initial repo setup

--- boolean.go
+++ boolean.go
@@ -10,3 +10,6 @@ import (
 
 // ExtractBoolean returns a 0/1 vector for the deterministic feature ID
 func ExtractBoolean(field, boolean string) (string, error) {
+
+	// deterministic collision resistant feature id
+	featureID := int32(murmur3.Sum32([]byte(uniqueHashPrefixStr + field)))
