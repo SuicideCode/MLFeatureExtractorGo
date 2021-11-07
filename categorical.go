f02fca8e149fee62687b3404341e264f2bdcac20
SuicideCode <113363871+SuicideCode@users.noreply.github.com>
2021-11-07 07:21:05 +0000

    merge with mpm

--- categorical.go
+++ categorical.go
@@ -6,3 +6,8 @@ import (
 
 	"github.com/spaolacci/murmur3"
 )
+
+// ExtractCategorical returns a vector that is a positive boolean for a
+// deterministic categorical feature ID
+func ExtractCategorical(field, category string) string {
+	featureID := int32(murmur3.Sum32([]byte(uniqueHashPrefixStr + field + category)))
