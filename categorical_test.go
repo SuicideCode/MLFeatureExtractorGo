61fb4733c848361dedb9522fc242574523412530
SuicideCode <113363871+SuicideCode@users.noreply.github.com>
2021-11-13 15:28:46 +0000

    Fixing conflicts

--- categorical_test.go
+++ categorical_test.go
@@ -12,3 +12,6 @@ func TestExtractCategorical(t *testing.T) {
 	expected := "-1661759407:1"
 
 	got := ExtractCategorical("username or something", "sodunn")
+
+	if diff := cmp.Diff(expected, got); diff != "" {
+		t.Errorf("unexpected difference: (-got +want)\n%s", diff)
