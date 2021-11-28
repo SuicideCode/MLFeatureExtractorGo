39ada627385654a2f4959a32f72e53b7d5cd5b3a
SuicideCode <113363871+SuicideCode@users.noreply.github.com>
2021-11-28 05:31:20 +0000

    docker stuff

--- const.go
+++ const.go
@@ -4,3 +4,5 @@ package featuremill
 const (
 	// this is used for stateless and determinitstic feature id
 	// generation using prefix+<user field>+context
+	uniqueHashPrefixStr = "I want features ids with low risk of collision"
+)
