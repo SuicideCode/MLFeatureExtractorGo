44a19d509b309b8eeb87eed3cd21a776d6b72e26
SuicideCode <113363871+SuicideCode@users.noreply.github.com>
2022-10-19 19:13:26 +0000

    Release version 2.1.0

--- ip.go
+++ ip.go
@@ -9,3 +9,7 @@ import (
 
 	"github.com/spaolacci/murmur3"
 )
+
+// ExtractIP returns a vector that is a scaled integer representation of IPv4 and IPv6 IPs
+// with a deterministic feature ID
+func ExtractIP(field, addr string) (string, error) {
