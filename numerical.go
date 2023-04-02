57e37764529e3056d22a4e535ad5f7c5e2fa7706
SuicideCode <113363871+SuicideCode@users.noreply.github.com>
2023-04-02 21:04:08 +0000

    fix import issue

--- numerical.go
+++ numerical.go
@@ -13,3 +13,6 @@ func ExtractNumericalMaxMin(field string, num float32, min float32, max float32)
 	featureID := int32(murmur3.Sum32([]byte(uniqueHashPrefixStr + field)))
 	return fmt.Sprintf("%d:%f", featureID, (num-min)/(max-min))
 }
+
+// ExtractNumerical returns a min/max scalled vector to a deterministic feature ID
+func ExtractNumerical(field string, num float32, min float32, max float32) string {
