1fd67fb7222d5b44265b171b049ae61e1901ff68
SuicideCode <113363871+SuicideCode@users.noreply.github.com>
2023-04-02 10:28:21 +0000

    replay

--- numerical.go
+++ numerical.go
@@ -6,3 +6,7 @@ import (
 	"math"
 
 	"github.com/spaolacci/murmur3"
+)
+
+// ExtractNumericalMaxMin returns a min/max scalled vector to a deterministic feature ID
+func ExtractNumericalMaxMin(field string, num float32, min float32, max float32) string {
