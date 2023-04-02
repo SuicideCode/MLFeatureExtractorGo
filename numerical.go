55946340661c5677b4c044dc8cb88d678c0e9793
SuicideCode <113363871+SuicideCode@users.noreply.github.com>
2023-04-02 15:32:29 +0000

    2003-10-06  Andrew Cagney  <cagney@redhat.com>

--- numerical.go
+++ numerical.go
@@ -10,3 +10,6 @@ import (
 
 // ExtractNumericalMaxMin returns a min/max scalled vector to a deterministic feature ID
 func ExtractNumericalMaxMin(field string, num float32, min float32, max float32) string {
+	featureID := int32(murmur3.Sum32([]byte(uniqueHashPrefixStr + field)))
+	return fmt.Sprintf("%d:%f", featureID, (num-min)/(max-min))
+}
