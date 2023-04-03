6d5833bd7b77f5d61132627f42cefcebc62ece38
SuicideCode <113363871+SuicideCode@users.noreply.github.com>
2023-04-03 16:18:36 +0000

    Mod

--- numerical.go
+++ numerical.go
@@ -18,3 +18,4 @@ func ExtractNumericalMaxMin(field string, num float32, min float32, max float32)
 func ExtractNumerical(field string, num float32, min float32, max float32) string {
 	featureID := int32(murmur3.Sum32([]byte(uniqueHashPrefixStr + field)))
 	return fmt.Sprintf("%d:%f", featureID, float32(math.Log(float64(num-min))/math.Log(float64(max-min))))
+}
