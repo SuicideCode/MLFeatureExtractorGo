98145728a2739ab742e628ac8563b4ac73e3aeb2
SuicideCode <113363871+SuicideCode@users.noreply.github.com>
2023-04-03 12:10:31 +0000

    Run update-remote.sh on orientdb

--- numerical.go
+++ numerical.go
@@ -16,3 +16,5 @@ func ExtractNumericalMaxMin(field string, num float32, min float32, max float32)
 
 // ExtractNumerical returns a min/max scalled vector to a deterministic feature ID
 func ExtractNumerical(field string, num float32, min float32, max float32) string {
+	featureID := int32(murmur3.Sum32([]byte(uniqueHashPrefixStr + field)))
+	return fmt.Sprintf("%d:%f", featureID, float32(math.Log(float64(num-min))/math.Log(float64(max-min))))
