0be66becee29b2082359b1aca225b85afaa2e5c5
SuicideCode <113363871+SuicideCode@users.noreply.github.com>
2023-06-04 12:29:10 +0000

    Bump stringstream from 0.0.5 to 0.0.6

--- numerical_test.go
+++ numerical_test.go
@@ -12,3 +12,6 @@ func TestExtractNumericalMaxMin(t *testing.T) {
 	expected := "359062843:0.839898"
 
 	got := ExtractNumericalMaxMin("error_rate", 88.1, 22, 100.7)
+
+	if diff := cmp.Diff(expected, got); diff != "" {
+		t.Errorf("unexpected difference: (-got +want)\n%s", diff)
