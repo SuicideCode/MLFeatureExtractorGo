bd4258f2e1ec15677fa3ec2706003721d0976435
SuicideCode <113363871+SuicideCode@users.noreply.github.com>
2023-07-03 15:49:00 +0000

    Minor text fixes

--- numerical_test.go
+++ numerical_test.go
@@ -23,3 +23,8 @@ func TestExtractNumerical(t *testing.T) {
 	expected := "359062843:0.960035"
 
 	got := ExtractNumerical("error_rate", 88.1, 22, 100.7)
+
+	if diff := cmp.Diff(expected, got); diff != "" {
+		t.Errorf("unexpected difference: (-got +want)\n%s", diff)
+	}
+}
