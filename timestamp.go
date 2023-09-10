0d2c2c23e33eef51952deeceff0fcf1af01f1cfc
SuicideCode <113363871+SuicideCode@users.noreply.github.com>
2023-09-10 13:01:06 +0000

    gpiolib: Don't return -EPROBE_DEFER to sysfs, or for invalid gpios

--- timestamp.go
+++ timestamp.go
@@ -7,3 +7,7 @@ import (
 	"fmt"
 
 	"github.com/araddon/dateparse"
+	"github.com/spaolacci/murmur3"
+)
+
+// ExtractTimestamp returns a slice of 3 scaled seasonality vectors: minute of hour, hour of day, day of week
